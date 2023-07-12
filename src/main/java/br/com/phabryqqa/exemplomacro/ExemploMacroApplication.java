9d2caf4463f211bcad1b5dfc0d40f677e0f0e0d2
Heredium Search <sysgenerator@gmail.com>
2023-07-12 16:52:45 +0100

    Primeiro Commit

---
+++ src/main/java/br/com/phabryqqa/exemplomacro/ExemploMacroApplication.java
@@ -0,0 +1,13 @@
+package br.com.phabryqqa.exemplomacro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploMacroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploMacroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/phabryqqa/exemplomacro/ExemploMacroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phabryqqa.exemplomacro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMacroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
